d92b5cf31ca9ec66c0ccdc9d0112a5ea85e03b5c
jjong0202 <jjong0202@naver.com>
2025-09-20 11:52:11 +0900

    222222222

---
+++ frontend/src/scripts/router.js
@@ -0,0 +1,23 @@
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Cart from "@/pages/Cart";
+import Order from "@/pages/Order";
+import Orders from "@/pages/Orders";
+import Test from "@/pages/Test";
+import {createRouter, createWebHistory} from "vue-router/dist/vue-router";
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/login', component: Login},
+    {path: '/cart', component: Cart},
+    {path: '/order', component: Order},
+    {path: '/orders', component: Orders},
+    {path: '/test', component: Test}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
